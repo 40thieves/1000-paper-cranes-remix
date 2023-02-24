b63ff817584e973dfa113ab7f2ca484ad8e2118c
Alasdair Smith <ali@alasdairsmith.co.uk>
2023-02-24 22:04:11 +0000

    Add footer styles

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,7 +3,7 @@ import GithubIcon from "./github-icon";
 export default function Footer() {
   return (
     <>
-      <footer>
+      <footer className="footer__container">
         <a
           className="footer__github"
           href="https://github.com/CodeYourFuture/1000-Paper-Cranes/"
@@ -12,8 +12,7 @@ export default function Footer() {
           <span className="sr-only">Find us on GitHub</span>
         </a>
         <p>
-          A project of
-          <a href="https://codeyourfuture.io">Code Your Future</a>
+          A project of <a href="https://codeyourfuture.io">Code Your Future</a>
           <br />
           Licensed under a Creative Commons Attribution-ShareAlike 4.0
           International License.
