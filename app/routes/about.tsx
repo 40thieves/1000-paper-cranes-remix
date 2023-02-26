a7b5b8613b759ef516fa867594caec469578811a
Alasdair Smith <ali@alasdairsmith.co.uk>
2023-02-26 21:33:47 +0000

    Extract main container to base
    This allows removal of index styles in about/gallery

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,15 +1,7 @@
-import { type LinksFunction } from "@remix-run/node";
-
 import Footer from "~/components/footer";
 import Header from "~/components/header";
 
 import Content from "~/data/about.md";
-
-import indexStylesUrl from "~/styles/index.css";
-
-export let links: LinksFunction = () => [
-  { rel: "stylesheet", href: indexStylesUrl },
-];
 
 export default function About() {
   return (
--- app/routes/gallery.tsx
+++ app/routes/gallery.tsx
@@ -1,15 +1,7 @@
-import { type LinksFunction } from "@remix-run/node";
-
 import Footer from "~/components/footer";
 import Header from "~/components/header";
 
 import Content from "~/data/gallery.mdx";
-
-import indexStylesUrl from "~/styles/index.css";
-
-export let links: LinksFunction = () => [
-  { rel: "stylesheet", href: indexStylesUrl },
-];
 
 export default function Gallery() {
   return (
