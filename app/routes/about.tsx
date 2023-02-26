ae1ec36741b8b838dbc64ecc643552513bc897ce
Alasdair Smith <ali@alasdairsmith.co.uk>
2023-02-26 22:19:36 +0000

    Add prose styling

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,14 +1,21 @@
+import { type LinksFunction } from "@remix-run/react/dist/routeModules";
 import Footer from "~/components/footer";
 import Header from "~/components/header";
 
 import Content from "~/data/about.md";
+
+import proseStylesUrl from "~/styles/prose.css";
+
+export let links: LinksFunction = () => [
+  { rel: "stylesheet", href: proseStylesUrl },
+];
 
 export default function About() {
   return (
     <>
       <Header />
       <main id="main" className="main__container">
-        <article>
+        <article className="prose">
           <Content />
         </article>
       </main>
--- app/routes/gallery.tsx
+++ app/routes/gallery.tsx
@@ -1,14 +1,21 @@
+import { type LinksFunction } from "@remix-run/react/dist/routeModules";
 import Footer from "~/components/footer";
 import Header from "~/components/header";
 
 import Content from "~/data/gallery.mdx";
+
+import proseStylesUrl from "~/styles/prose.css";
+
+export let links: LinksFunction = () => [
+  { rel: "stylesheet", href: proseStylesUrl },
+];
 
 export default function Gallery() {
   return (
     <>
       <Header />
       <main id="main" className="main__container">
-        <article>
+        <article className="prose">
           <Content />
         </article>
       </main>
