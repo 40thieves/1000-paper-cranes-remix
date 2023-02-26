eb110d3dc749cd66a50b595def66badf15ec8541
Alasdair Smith <ali@alasdairsmith.co.uk>
2023-02-26 20:24:58 +0000

    Add about content by loading from markdown data file

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,4 +1,7 @@
-import { type LinksFunction } from "@remix-run/node";
+import { json, type LoaderFunction, type LinksFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+import { getHTMLFromMarkdown } from "~/utils/markdown.server";
 
 import Footer from "~/components/footer";
 import Header from "~/components/header";
@@ -9,11 +12,22 @@ export let links: LinksFunction = () => [
   { rel: "stylesheet", href: indexStylesUrl },
 ];
 
+export let loader: LoaderFunction = async () => {
+  const html = await getHTMLFromMarkdown("about.md");
+  return json({
+    html,
+  });
+};
+
 export default function Index() {
+  const { html } = useLoaderData<typeof loader>();
+
   return (
     <>
       <Header />
-      <main id="main" className="main__container"></main>
+      <main id="main" className="main__container">
+        <article dangerouslySetInnerHTML={{ __html: html }} />
+      </main>
 
       <Footer />
     </>
